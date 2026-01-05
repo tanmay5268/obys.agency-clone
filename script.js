8747232ec0a9e89c627947ac82d3af4e511f2d7a
tanmay5268 <tanmay.mewati94@gmail.com>
2026-01-05 18:15:59 +0530

    Enhance navigation and cursor animations;

--- script.js
+++ script.js
@@ -27,16 +27,43 @@ function loadingAnimation() {
     tl.to("#loader", {
         opacity: 0,
         duration: 0.5,
-        delay: 3.6,
+        delay: 1,
+        
     })
     tl.from("#page1", {
         delay: 0.2,
         y: 1600,
-        opacity: 0,
+        duration: 0.6,
         ease: Power4
     })
     tl.to("#loader", {
         display: "none",
     })
+    tl.from("#nav", {
+        opacity:0,
+    })
+    tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1", {
+        y: 100,
+        stagger: 0.25
+    })
+    
 }
 loadingAnimation();
+
+function cursorAnimation() {
+    document.addEventListener("mousemove", function (dets) {
+        gsap.to("#crsr", {
+            left: dets.x,
+            top: dets.y,
+
+        })
+    });
+
+
+    Shery.makeMagnet("#nav-part2 h4, .brand__svg" /* Element to target.*/, {
+        //Parameters are optional.
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 1,
+    });
+}
+cursorAnimation();
